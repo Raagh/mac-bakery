5f9946a45fb5aa090883b172e59e8e8db708ff95
Patricio Ferraggi <pattferraggi@gmail.com>
2018-06-14 22:02:03 +0200

    finishing home-intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BoxComponent } from './components/box/box.component';
+
 import { HomeComponent } from './views/home/home.component';
+import { HomeIntroComponent } from './views/home/home-intro/home-intro.component';
+import { HomeBoxesComponent } from './views/home/home-boxes/home-boxes.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
-        HomeComponent
+        BoxComponent,
+        HomeComponent,
+        HomeIntroComponent,
+        HomeBoxesComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/views/home/home-boxes/home-boxes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-boxes',
+  templateUrl: './home-boxes.component.html',
+  styleUrls: ['./home-boxes.component.scss']
+})
+export class HomeBoxesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
