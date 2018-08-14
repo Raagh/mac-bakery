cfea85f52759cb27be81bb534a7036cdea641db2
Patricio Ferraggi <pattferraggi@gmail.com>
2018-08-14 21:20:17 +0200

    finished second navigation and added burger toggle on mobile

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    public BurgerToggled: boolean = false;
 
+    ngOnInit() {
+    }
+
+    toggleBurger() {
+        if (this.BurgerToggled)
+            this.BurgerToggled = false;
+        else
+            this.BurgerToggled = true;
+    }
 }
